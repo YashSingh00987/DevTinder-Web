acc92a18fe81335ac2960f2ddd9562025e106791
YashSingh00987 <mohitsingh00987@gmail.com>
2024-11-11 14:16:03 +0530

    refactor login component

--- src/Login .jsx
+++ src/Login .jsx
@@ -1,5 +1,66 @@
+import axios from "axios";
+import { useState } from "react";
+
 const Login = () => {
-  return <div>Login </div>;
+  const [email, setEmail] = useState("bolivia@gmail.com");
+  const [password, setPassword] = useState("Bolivia@123");
+
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:7777/login",
+        {
+          email,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      console.log("Login successful:", response.data);
+    } catch (err) {
+      console.error("Login error:", err.response?.data || err.message);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center my-8">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <div>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Email ID:</span>
+              </div>
+              <input
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">Password</span>
+              </div>
+              <input
+                type="text"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="input input-bordered w-full max-w-xs"
+              />
+            </label>
+          </div>
+          <div className="card-actions justify-center m-2">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
