99d872865ec2cdd2773ea05e0950593962a873d7
YashSingh00987 <mohitsingh00987@gmail.com>
2024-11-13 20:05:09 +0530

    Created profile section & updated card for user & added connections for a particular user & impove some bugs

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,21 +1,19 @@
+/* eslint-disable react/prop-types */
 const UserCard = ({ user }) => {
-  const { firstName, lastName, age, about, gender, skills, photoUrl } = user;
+  const { firstName, lastName, age, about, gender, photoUrl, skills } = user;
   console.log(user);
   return (
-    <div className="card bg-base-300 w-96 shadow-xl">
-      <figure className="pt-2">
-        <img
-          src={photoUrl}
-          alt="user"
-          height={"200px"}
-          width={"300px"}
-          className="rounded-md"
-        />
+    <div className="card bg-base-300 w-96 shadow-xl h-[600px]">
+      <figure>
+        <img src={photoUrl} alt="user" className="rounded-md" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{`${firstName} ${lastName}`}</h2>
-        {age && gender && <p>{`${age}, ${gender}`}</p>}
-        <p>{about}</p>
+        <div className="h-20">
+          {age && gender && <p>{`${age}, ${gender}`}</p>}
+          {skills && <p>{`Skills: ${skills}`}</p>}
+          <p>{about}</p>
+        </div>
         <div className="card-actions justify-center py-2">
           <button className="btn btn-primary">Ignore</button>
           <button className="btn btn-secondary">Interested</button>
